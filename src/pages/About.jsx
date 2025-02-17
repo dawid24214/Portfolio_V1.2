43d71f834d8d8c89b4d3fccf63d0f9fe2646aa72
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-17 17:10:05 +0100

    Installation sass embedded

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,10 @@ import {React} from "react";
 
  export const About = () =>{
     return(
-        <>
+        <div>
             <p>
-                Groszek 
+                Groszek
             </p>
-        </>
+        </div>
     )
 }
