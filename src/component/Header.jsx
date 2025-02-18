3b0814fb4ed584ed52f8eacda942f5cc576537f5
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-18 18:19:14 +0100

    Header
    Styling in the Header file and modifying the JSX file to transform it into hamburger menu structure with resnponsiveness.

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,13 +1,45 @@
-import React from "react";
-import {Link} from "react-router-dom";
+// import React from "react";
+// import {Link} from "react-router-dom";
+// import '../styles/_header.scss';
+//
+// export const Header = () =>{
+//     return (
+//         <header className="header">
+//             <nav>
+//                 <ul className="nav-links">
+//                     <li><Link to='/Home'>Strona Główna</Link></li>
+//                     <li><Link to="/About">O mnie</Link></li>
+//                     <li><Link to="/Projects">Projekty</Link></li>
+//                     <li><Link to="/Portfolio">Portfolio</Link></li>
+//                     <li><Link to="/Contact">Kontakt</Link></li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     )
+// }
+//
+//
+//
 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
+export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-export const Header = () =>{
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className="header">
             <nav>
-                <ul className="nav-links">
+                <div className="hamburger" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <ul className={`nav-links ${isOpen ? "active" : ""}`}>
                     <li><Link to='/Home'>Strona Główna</Link></li>
                     <li><Link to="/About">O mnie</Link></li>
                     <li><Link to="/Projects">Projekty</Link></li>
@@ -16,5 +48,7 @@ export const Header = () =>{
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
+
+
